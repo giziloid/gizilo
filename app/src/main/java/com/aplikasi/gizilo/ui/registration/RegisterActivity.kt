e4406216dd800f73bf87387a3bddbc3536ccfc52
haikalbasri <basribusrut93@gmail.com>
2024-05-28 00:50:41 +0700

    CBH 58 Making layout Menu Register

--- app/src/main/java/com/aplikasi/gizilo/ui/registration/RegisterActivity.kt
+++ app/src/main/java/com/aplikasi/gizilo/ui/registration/RegisterActivity.kt
@@ -1,4 +1,4 @@
-package com.aplikasi.gizilo.ui
+package com.aplikasi.gizilo.ui.registration
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.aplikasi.gizilo.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
