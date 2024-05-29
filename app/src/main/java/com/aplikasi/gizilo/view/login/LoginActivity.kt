dbd17bb86ed76f219e43c895f7bdb306b0dfaae3
haikalbasri <basribusrut93@gmail.com>
2024-05-29 21:56:56 +0700

    implement bottomnavigation home camera and profile some features was removed by me and icon that not suitable put in there

--- app/src/main/java/com/aplikasi/gizilo/view/login/LoginActivity.kt
+++ app/src/main/java/com/aplikasi/gizilo/view/login/LoginActivity.kt
@@ -1,21 +1,29 @@
-package com.aplikasi.gizilo.ui.settings
+package com.aplikasi.gizilo.view.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.aplikasi.gizilo.R
+import com.aplikasi.gizilo.databinding.ActivityLoginBinding
+import com.aplikasi.gizilo.view.MainActivity
 
-class SettingActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_setting)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.buttonLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/aplikasi/gizilo/view/settings/SettingActivity.kt
+++ app/src/main/java/com/aplikasi/gizilo/view/settings/SettingActivity.kt
@@ -1,4 +1,4 @@
-package com.aplikasi.gizilo.ui.settings
+package com.aplikasi.gizilo.view.settings
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.aplikasi.gizilo.R
 
-class AboutUsActivity : AppCompatActivity() {
+class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_about_us)
+        setContentView(R.layout.activity_setting)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
